a0c5266144fa29bbeea2fc107a45b308a1839da0
Dinesh N Perera <dineshnp76@gmail.com>
2026-05-18 17:49:02 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ document.addEventListener('DOMContentLoaded', function() {
         offset: 100
     });
     
-function showNotification(message) {
+function showNotification(                                placeholder="Welcome to DNP Tourism — Experience Luxury Travel Across Sri Lanka, 🏖️ Explore Beaches, Wildlife & Cultural Wonders with Personalized Tours.
+) {
   const liveRegion = document.getElementById("liveNotification");
 
   // Clear first
